a0a90534b9402e35afeb20d7a090eebd59048ad7
jonrubiaa62242046-stack <jonrubia.a62242046@umak.edu.ph>
2026-06-05 16:38:52 +0800

    jp

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,12 +32,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${bricolage.variable} ${space.variable} ${vietnam.variable}`}>
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-          rel="stylesheet"
-        />
-      </head>
       <body className="min-h-screen bg-background text-on-background font-body-md">{children}</body>
     </html>
   );
